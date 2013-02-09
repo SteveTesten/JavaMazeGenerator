c066b3d473c5789894f49af6c4135e1e68ed8412
Steve Testen <testen@gmail.com>
2013-02-08 22:41:36 -0500

    Initial Grid Structure Complete

--- MazeGenerator/src/mazegen/GenerateMaze.java
+++ MazeGenerator/src/mazegen/GenerateMaze.java
@@ -4,38 +4,76 @@ import java.lang.Integer;
 
 public class GenerateMaze {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 		if (args.length==2)
 		{
 			System.out.println("Maze dimensions: " + args[0] + " by " + args[1]);
 			int rows = Integer.parseInt(args[0]);
 			int columns = Integer.parseInt(args[1]);
 			
-			String[][] Grid = formGrid(rows,columns);
+			String[][] Grid = formGridStructure(rows,columns);
 			
-			System.out.println(Grid[2][1]);
+			int a = 0;
+			int b = 0;
 			
+			for (a=0;a<rows;a++)
+			{
+				for (b=0;b<(columns-1);b++)
+				{
+					System.out.print(Grid[a][b] + " ");
+				}
+				System.out.println(Grid[a][b]);
+			}
 		}
 		else
-		{
 			System.out.println("Maze dimensions not entered properly! Try again with with args 'x y'.");
-		}
 			
 	}
 	
-	public static String[][] formGrid (int rows,int columns)
+	public static String[][] formGridStructure (int rows,int columns)
 	{
+		/*
+		 * This method forms the structure of the grid.  Each cell of
+		 * RxC array contains the information about the surrounding
+		 * walls of that cell.  Each cell is 4 values, describing the
+		 * North, East, South, and West walls, respectively.  The value
+		 * describes the state: 0=No Wall, 1 = Wall, 2 = Border.
+		 */
+		
 		String[][] GridStructure = new String[rows][columns];
+		
 		for(int i=0;i<rows;i++)
 		{
 			for(int j=0; j<columns;j++)
 			{
-				GridStructure[i][j] = new String("1111");
+				if (i==0)
+				{
+					if (j==0)
+						GridStructure[i][j] = new String("2112");
+					else if (j==(columns-1))
+						GridStructure[i][j] = new String("2211");
+					else
+						GridStructure[i][j] = new String("2111");
+				}
+				else if (i==(rows-1))
+				{
+					if (j==0)
+						GridStructure[i][j] = new String("1122");
+					else if (j==(columns-1))
+						GridStructure[i][j] = new String("1221");
+					else
+						GridStructure[i][j] = new String("1121");
+				}
+				else
+				{
+					if (j==0)
+						GridStructure[i][j] = new String("1112");
+					else if (j==(columns-1))
+						GridStructure[i][j] = new String("1211");
+					else
+						GridStructure[i][j] = new String("1111");
+				}
 			}
 		}
 		
